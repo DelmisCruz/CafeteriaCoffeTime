9c3b411ae93cb352035b268cad39638479bc394c
Delmis Cruz Rosa <delmismarisela17@gmail.com>
2022-02-17 17:17:10 -0600

    Agregando Arquitectura Basica

---
+++ CoffeeTime/CoffeeTime.BL/ProductosBL.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeTime.BL
+{
+    public class ProductosBL
+    {
+        public List<Producto> ObtenerProductos()
+        {
+            var producto1 = new Producto();
+            producto1.Id = 1;
+            producto1.Descripcion = "Cafe Negro";
+            producto1.Precio = 35;
+
+            var producto2 = new Producto();
+            producto2.Id = 2;
+            producto2.Descripcion = "Chocolate";
+            producto2.Precio = 40;
+
+            var listadeProductos = new List<Producto>();
+            listadeProductos.Add(producto1);
+            listadeProductos.Add(producto2);
+
+            return listadeProductos;
+        }
+    }
+}
--- CoffeeTime/CoffeeTime.Web/Controllers/ProductosController.cs
+++ CoffeeTime/CoffeeTime.Web/Controllers/ProductosController.cs
@@ -1,4 +1,4 @@
-﻿using CoffeeTime.Web.Models;
+﻿using CoffeeTime.BL;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,17 +12,8 @@ namespace CoffeeTime.Web.Controllers
         // GET: Productos
         public ActionResult Index()
         {
-            var producto1 = new ProductoModel();
-            producto1.Id = 1;
-            producto1.Descripcion = "Cafe Negro";
-
-            var producto2 = new ProductoModel();
-            producto2.Id = 2;
-            producto2.Descripcion = "Chocolate";
-
-            var listadeProductos = new List<ProductoModel>();
-            listadeProductos.Add(producto1);
-            listadeProductos.Add(producto2);
+            var productosBL = new ProductosBL();
+            var listadeProductos = productosBL.ObtenerProductos();
 
             return View(listadeProductos);
         }
