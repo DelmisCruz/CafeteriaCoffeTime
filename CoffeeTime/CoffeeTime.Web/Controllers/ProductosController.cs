cc4fc0d3c5d5a181999e192614a3c988dbfbf5c0
Delmis Cruz Rosa <delmismarisela17@gmail.com>
2022-02-17 16:33:25 -0600

    Agregando Lista de productos

---
+++ CoffeeTime/CoffeeTime.Web/Controllers/ProductosController.cs
@@ -0,0 +1,30 @@
+﻿using CoffeeTime.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CoffeeTime.Web.Controllers
+{
+    public class ProductosController : Controller
+    {
+        // GET: Productos
+        public ActionResult Index()
+        {
+            var producto1 = new ProductoModel();
+            producto1.Id = 1;
+            producto1.Descripcion = "Cafe Negro";
+
+            var producto2 = new ProductoModel();
+            producto2.Id = 2;
+            producto2.Descripcion = "Chocolate";
+
+            var listadeProductos = new List<ProductoModel>();
+            listadeProductos.Add(producto1);
+            listadeProductos.Add(producto2);
+
+            return View(listadeProductos);
+        }
+    }
+}
